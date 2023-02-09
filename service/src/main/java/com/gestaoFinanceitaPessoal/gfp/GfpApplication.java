3cb714a56725b32d4a8c4df41bce73e1e34caabc
layla.couto <layla.couto@basis.com.br>
2023-02-09 09:44:41 -0300

    iniciando o projeto

---
+++ service/src/main/java/com/gestaoFinanceitaPessoal/gfp/GfpApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoFinanceitaPessoal.gfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfpApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/com/gestaoFinanceitaPessoal/gfp/GfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaoFinanceitaPessoal.gfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
